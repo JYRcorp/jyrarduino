3f1e936a1266ae91f747de63a30adf6ae6b1f9f6
JYRcorp <jy.rousselle@free.fr>
2017-09-02 16:37:01 +0200

    Utilisation de A5

--- i2c_slave/README.md
+++ i2c_slave/README.md
@@ -1,10 +1,10 @@
 # i2c_slave
-Sur "arduino pro mini" il y a 5 entrées analogiques A0..A4
+Sur "arduino pro mini" il y a 6 entrées analogiques A0 à A5.
 
-Le protocole I2C utilise 2 des entrées analogiques : A3 et A4
+Le protocole I2C utilise 2 des entrées analogiques : A3 et A4.
 
-On ne peut donc utiliser, au travers de I2C, que 3 entrées analogiques A0..A2
+On ne peut donc utiliser, au travers de I2C, que 4 entrées analogiques A0, A1, A2 et A5
 
-Cet exemple montre que la tentative d'utilisation d'une 4eme entrée A4 est un échec.
+Cet exemple montre que la tentative d'utilisation de A4 est un échec.
 
-La solution est d'utiliser l'autre bus disponible SPI.
+Pour disposer des 6 entrées analogiques, la solution est d'utiliser l'autre bus disponible : SPI.
--- i2c_slave/i2c_slave.ino
+++ i2c_slave/i2c_slave.ino
@@ -1,20 +1,20 @@
 //
-// Lecture analogique de A0, A1, A2, A4
-// + renvoi des données en I2C
+// Lecture analogique de A0, A1, A2, A5
+// + renvoi des données en I2C (utilisant A3 et A4)
 //
 #include <Wire.h>
 // I2C address : choisir de 0x03 à 0x77 
 #define SLAVE_ADDRESS 0x12
 int readData=0;
-char data[12];         // 6 lectures analogiques possibles dans 6 int = 12 bytes
-byte NBPINBYTES=12;  
-// LED
+char data[8];         // 4 lectures analogiques possibles dans 4 int = 8 bytes
+byte NBPINBYTES=8;  
+// clignotement de la LED
 int boucle=0;
 int MAXBOUCLE=180;
 int MIDBOUCLE=160;
 
 void setup() {
-    // initialize digital pin LED_BUILTIN as an output.
+    // LED : initialize digital pin LED_BUILTIN as an output.
     pinMode(LED_BUILTIN, OUTPUT);    
     // pins analogiques
     pinMode(A0, INPUT );      // ensure button pin is an input
@@ -23,8 +23,8 @@ void setup() {
     digitalWrite(A1, LOW );   // ensure pullup is off on button pin
     pinMode(A2, INPUT );      // ensure button pin is an input
     digitalWrite(A2, LOW );   // ensure pullup is off on button pin
-    pinMode(A4, INPUT );      // ensure button pin is an input
-    digitalWrite(A4, LOW );   // ensure pullup is off on button pin
+    pinMode(A5, INPUT );      // ensure button pin is an input
+    digitalWrite(A5, LOW );   // ensure pullup is off on button pin
     // I2C
     Wire.begin(SLAVE_ADDRESS); // join i2c bus (address optional for master)
     Wire.onReceive(receiveData); // register event
@@ -57,7 +57,7 @@ void loop() {
     data[4] = (byte) (tmpval >> 8);    // high order byte
     data[5] = (byte) (tmpval & 0xFF);  // low-order byte   
     
-    tmpval = analogRead(A4);
+    tmpval = analogRead(A5);
     data[6] = (byte) (tmpval >> 8);    // high order byte
     data[7] = (byte) (tmpval & 0xFF);  // low-order byte   
     
@@ -65,7 +65,7 @@ void loop() {
 }
 
 void receiveData(int byteCount){
-  // Reception de l'indice du PIN demandé (A0 byte1 =0; A0 byte2 =1...  A1 ... A2 ...  ya pas A3  mais A4 )
+  // Reception de l'indice du PIN demandé (A0/byte1 =0; A0/byte2 =1...  A1 ... A2 ...  ya pas A3 ni A4 mais A5)
     while(Wire.available()) {
       readData = Wire.read();
     }
